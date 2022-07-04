0b65b0210e790ec2327b14976b086b7ac7f3b26a
Alex4Rain <alex4rain@gmail.com>
2022-07-04 16:18:16 +0300

    complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean == bean2);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
